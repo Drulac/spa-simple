d1d01766c4f6bf7ae45a4205402fb044fe24f813
drulac <drulac@protonmail.com>
2017-10-14 12:55:36 +0200

    delete console outputs

--- front/route.js
+++ front/route.js
@@ -6,10 +6,8 @@ const templateCache = {};
 
 const req = (url, data)=>
 new Promise(async (resolve, reject)=>{
-	console.time('get '+url+' from network');
 	if(url in templateCache)
 	{
-		console.timeEnd('get '+url+' from network');
 		resolve(templateCache[url]);
 	}else{
 		const request = new XMLHttpRequest();
@@ -21,10 +19,8 @@ new Promise(async (resolve, reject)=>{
 			{
 				if(request.status === 200){
 					templateCache[url] = request.response;
-					console.timeEnd('get '+url+' from network');
 					resolve(request.response);
 				}else{
-					console.timeEnd('get '+url+' from network');
 					reject(request.status);
 				}
 			}
@@ -33,34 +29,18 @@ new Promise(async (resolve, reject)=>{
 });
 
 const loadPage = async uri=>{
-	console.time(uri);
 	setUrl(uri);
 
-	console.time('network requests');
 	const [templateCode, data] = await Promise.all([
 		req(uri+'.tpl'),
-		new Promise(async (resolve, reject)=>{
-			console.time('get '+uri+' from api socket');
-			const data = await socket.get(uri, []);
-			console.timeEnd('get '+uri+' from api socket');
-			resolve(data);
-		})
+		socket.get(uri, [])
 	]);
-	console.timeEnd('network requests');
 
-	console.log(data);
-
-	console.time('func and data');
 	const props = Object.getOwnPropertyNames(data);
 	const func = new Function(...props, 'return `'+templateCode+'`');
 	const args = props.map(prop=>data[prop]);
-	console.timeEnd('func and data');
 
-	console.time('set innerHTML');
 	document.body.innerHTML = (func)(...args);
-	console.timeEnd('set innerHTML');
-	console.timeEnd(uri);
-	console.log('-------------------');
 };
 
 socket.onopen = async ()=>{
